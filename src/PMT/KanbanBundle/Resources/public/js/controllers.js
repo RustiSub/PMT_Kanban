d4b4cc1d63a68576affa8842167a223a53b5c90a
wouter pirotte <wpi@anaxis.be>
2013-11-21 18:13:13 +0100

    -Intial setup and test of Angular implementation.

--- src/PMT/KanbanBundle/Resources/public/js/controllers.js
+++ src/PMT/KanbanBundle/Resources/public/js/controllers.js
@@ -1,5 +1,26 @@
 'use strict';
 
+function allowDrop(ev)
+{
+  ev.preventDefault();
+}
+
+function drag(ev)
+{
+  ev.dataTransfer.setData("Text",ev.target.id);
+}
+
+function drop(ev)
+{
+  ev.preventDefault();
+  var data=ev.dataTransfer.getData("Text");
+
+  var parentElement = $(ev.target).closest('ul');
+  var dataElement = $('#' + data);
+
+  parentElement.append(dataElement);
+}
+
 /* Controllers */
 
 var boardApp = angular.module('boardApp', []);
@@ -15,6 +36,7 @@ boardApp.controller('TicketCtrl', function($scope, $http) {
   for (var i = 0; i < 10; i++) {
     $scope.items.push(
       {
+        'id': i,
         'title': 'Ticket ' + i,
         'description': 'Dit is een ng beschrijving ' + i
       }
